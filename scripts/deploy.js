1aa92607aa0ffd9963f071c11c91d637bba0300d
drdarian <dariantaban@gmail.com>
2021-06-21 11:33:07 -0700

    Environment and token built

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  const FCTToken = await hre.ethers.getContractFactory("FCTToken");
+  const fctToken = await FCCToken.deploy("FCTToken", "TKN");
+
+  await fctToken.deployed();
+
+  console.log("Token deployed to:", fctToken.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
